b2579a16bd28233868a55850bf81fc2d563aec0d
Bshaley25 <bshaley25@gmail.com>
2020-01-22 10:46:16 -0700

    created hiker.rb, trail.rb, trip.rb in lib/models to hold those models migrated and created trails table and colorado_hiking_trails.db

---
+++ db/migrate/20200122164258_create_trails_table.rb
@@ -0,0 +1,14 @@
+class CreateTrailsTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :trails do |t|
+      t.string :name
+      t.string :region 
+      t.decimal :miles
+      t.string :difficulty
+      t.string :type
+      t.string :link
+      t.string :keywords
+      t.text :notes
+    end
+  end
+end
