6be77c7422d7068eaff49db93e905caa976a0d4d
github username <github email address>
2020-01-23 16:11:15 -0700

    Added the ability to create a favorites list and delete trips

--- lib/models/trip.rb
+++ lib/models/trip.rb
@@ -7,5 +7,19 @@ class Trip < ActiveRecord::Base
         @@prompt = TTY::Prompt.new
     end
     
+    def self.user_trails(user)
+        trips_by_user = all.select do |trip|
+            trip.hiker == user 
+        end 
+        trips_by_user.map do |trip|
+            trip.trail.name
+        end
+    end 
+
+    def self.find_trip_object(hiker,trail)
+        all.find do |trip|
+            trip.hiker == hiker && trip.trail == trail
+        end 
+    end 
     
 end
